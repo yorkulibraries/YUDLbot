7b299579cb5c588865953b21949de9927e5a824b
nruest <ruestn@gmail.com>
2014-05-25 11:21:15 -0400

    Use config.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # YUDLbot
 
 A silly little bot that tweets random [York University Digital Library](http://digital.library.yorku.ca) content. You can find me at [@YUDLbot](http://twitter.com/YUDLbot).
+
+If you want use it, you will need to setup the variables outlined in `config.py.example` and rename it to `config.py`. For `SOLR_DATA` you will need to construct a query, and append `&fl=PID%2C+mods_titleInfo_title_ms&wt=json&indent=true' along with the number of row you want. For example `&rows=100`.
--- YUDLbot.py
+++ YUDLbot.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tweepy, time, sys, json, urllib, random, bitly_api
+import tweepy, time, sys, json, urllib, random, bitly_api, config
 
-CONSUMER_KEY = 'something'
-CONSUMER_SECRET = 'something'
-ACCESS_KEY = 'something'
-ACCESS_SECRET = 'something'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
-BITLY_API_USER = "something"
-BITLY_API_KEY = "something"
+b = bitly_api.Connection(config.BITLY_API_USER, config.BITLY_API_KEY)
 
-b = bitly_api.Connection(BITLY_API_USER, BITLY_API_KEY)
-
-url = 'http://localhost/solr/yudl/select?q=YOURSOLRQUERY&wt=json&indent=true'
-
-response = urllib.urlopen(url);
+response = urllib.urlopen(config.SOLR_DATA);
 data = json.loads(response.read())
 docs = data["response"]["docs"]
 
 items = random.sample(docs,1)
-#response.close()
 
 for item in items:
   pid = item["PID"]
@@ -32,5 +22,5 @@ for item in items:
   url += pid
   shorten_url = b.shorten(url)
   bitly_url = shorten_url["url"]
-  tweet_text = z = "%s %s" % (title,bitly_url)
+  tweet_text = "%s %s" % (title,bitly_url)
   api.update_status(tweet_text)
