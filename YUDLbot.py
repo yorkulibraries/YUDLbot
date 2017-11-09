f36f1efbd048004d1b3adca12b71f1a7afae8f9b
nruest <ruestn@gmail.com>
2017-11-09 09:21:30 -0500

    Longer tweets.

--- YUDLbot.py
+++ YUDLbot.py
@@ -22,13 +22,13 @@ items = random.sample(docs,1)
 for item in items:
   pid = item["PID"]
   description = item["mods_abstract_s"][0]
-  if len(description) > 117:
-    while len(description) + 3 > 116:
+  if len(description) > 257:
+    while len(description) + 3 > 256:
         description = description[:len(description) - 1]
     description = description + '...'
   title = item["mods_titleInfo_title_s"][0]
-  if len(title) > 117:
-    while len(title) + 3 > 116:
+  if len(title) > 257:
+    while len(title) + 3 > 256:
         title = title[:len(title) - 1]
     title = title + '...'
   url = "http://digital.library.yorku.ca/islandora/object/"
