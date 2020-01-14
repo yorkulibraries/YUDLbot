6076d124f5aee810d70fb72eb98289f781b5f3a9
nruest <ruestn@gmail.com>
2020-01-14 11:17:19 -0500

    Query updates for YUDL reindexing, and code tweaks.
    - Clean up Python code with black and isort
    - Change how Solr query is done
    - Update and rename queries
    - Update README
    - Update gitignore

--- README.md
+++ README.md
@@ -7,7 +7,17 @@ A bot that tweets random [York University Digital Library](http://digital.librar
 * [@YUDLcat](http://twitter.com/YUDLcat)
 * [@YUDLdog](http://twitter.com/YUDLdog)
 
-If you want use it, you will need to setup the variables outlined in `config.py.example` and rename it to `config.py`. For `SOLR_DATA` you will need to construct a query, and append `&fl=PID%2C+mods_titleInfo_title_ms&wt=json&indent=true` along with the number of rows you want. For example `&rows=100`. You can also crib from the example queries in the repo; [`YUDLbot-desc.query`](YUDLbot-desc.query) and [`YUDLbotCat.query`](YUDLbotCat.query).
+If you want use it, you will need to setup the variables outlined in `config.py.example` and rename it to `config.py`. You will also need a Solr query. These queries from an Islandora 7.x instance. Feel free to crib from the example queries in the repo:
+* [`yudlbot-cat.query`](yudlbot-cat.query)
+* [`yudlbot-desc.query`](yudlbot-desc.query)
+* [`yudlbot-dog.query`](yudlbot-dog.query)
+* [`yudlbot.query`](yudlbot.query)
+
+You can automate it with a cron job like so:
+
+```
+17 */5 * * * /usr/bin/python /path/to/YUDLbot.py
+```
 
 ## License
 
--- YUDLbot.py
+++ YUDLbot.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tweepy, time, sys, json, urllib, random, config
+import json
+import random
+import sys
+import time
+import urllib
+
+import config
+import tweepy
 
 auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
 auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
-response = urllib.urlopen(config.SOLR_DATA);
+solr_query = open("yudlbot.query", "r").read()
+
+response = urllib.urlopen(solr_query)
 data = json.loads(response.read())
 docs = data["response"]["docs"]
 
-items = random.sample(docs,1)
-
-response = urllib.urlopen(solr_data);
-data = json.loads(response.read())
-docs = data["response"]["docs"]
-
-items = random.sample(docs,1)
+items = random.sample(docs, 1)
 
 for item in items:
-  pid = item["PID"]
-  description = item["mods_abstract_s"][0]
-  if len(description) > 257:
-    while len(description) + 3 > 256:
-        description = description[:len(description) - 1]
-    description = description + '...'
-  title = item["mods_titleInfo_title_s"][0]
-  if len(title) > 257:
-    while len(title) + 3 > 256:
-        title = title[:len(title) - 1]
-    title = title + '...'
-  url = "http://digital.library.yorku.ca/islandora/object/"
-  url += pid
-  tweet_text = "%s %s" % (description,url)
-  api.update_status(tweet_text)
-
+    pid = item["PID"]
+    description = item["mods_abstract_s"][0]
+    if len(description) > 257:
+        while len(description) + 3 > 256:
+            description = description[: len(description) - 1]
+        description = description + "..."
+    title = item["mods_titleInfo_title_s"][0]
+    if len(title) > 257:
+        while len(title) + 3 > 256:
+            title = title[: len(title) - 1]
+        title = title + "..."
+    url = "http://digital.library.yorku.ca/islandora/object/"
+    url += pid
+    tweet_text = "%s %s" % (description, url)
+    api.update_status(tweet_text)
